c294b263a1dee8e34ee5738f8f90148cbc4ed78e
LorenzoJacome <lorenzo.jacome@hotmail.com>
2020-10-01 18:16:35 -0500

    Added Health bar
    Added UI component of health bar with a script

---
+++ Assets/Scripts/Bars.cs
@@ -0,0 +1,21 @@
+﻿/*
+By: Lorenzo Jácome Ceniceros
+Date: 01/10/2020
+Description: Code that controls the sliders of the player when he uses any of his habilities or hp
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bars : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void SetHealth(int health){
+        slider.value = health;
+    }
+}
