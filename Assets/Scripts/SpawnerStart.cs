54c9d5605e14f513776d3f3fc992f294d6eacd60
Maxsafer <42416882+Maxsafer@users.noreply.github.com>
2020-10-14 01:47:28 -0500

    level gen

---
+++ Assets/Scripts/SpawnerStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerStart : MonoBehaviour
+{
+    public GameObject start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(start, transform.position, Quaternion.identity);
+    }
+}
