3705e13c3f08f5056e35beb5c1e7bdcbfd8b5e2a
LorenzoJacome <lorenzo.jacome@hotmail.com>
2020-10-15 17:35:45 -0500

    Drone texture and script

---
+++ Assets/Scripts/droneScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class droneScript : MonoBehaviour
+{
+    [SerializeField] public float speed;
+    public Transform obj;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 tempVect = new Vector3(speed, 0, 0);
+        tempVect = tempVect.normalized * speed * Time.deltaTime;
+        obj.transform.position += tempVect;
+    }
+}
