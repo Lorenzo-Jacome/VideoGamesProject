32a3c95c15fcc94a128b075c39f8ed9c5c45aba2
Lorenzo Jácome <lorenzo.jacome@hotmail.com>
2020-10-28 22:10:31 -0600

    Update TimeToLive.cs

--- Assets/Scripts/TimeToLive.cs
+++ Assets/Scripts/TimeToLive.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿
+/*
+By: Maximiliano Sapién
+Description: Mainly used to specify the duration of a bullet in the world.
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
