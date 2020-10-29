fde8a02abc104ab72cd9559bb5dac7a496d94a31
Lorenzo Jácome <lorenzo.jacome@hotmail.com>
2020-10-28 22:17:44 -0600

    Update enemBulletMov.cs

--- Assets/Scripts/enemBulletMov.cs
+++ Assets/Scripts/enemBulletMov.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿
+/*
+By: Maximilianto Sapién
+Description: Manages the enemy's bullet: Movement and collition
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
